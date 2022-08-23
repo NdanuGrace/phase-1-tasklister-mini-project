a6d2aac61f6fbe043c1b5838f8cfabc3f5fe1ea7
gracekyalo <gracendanu27@gmail.com>
2022-08-23 15:04:04 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit' ,(e) =>{
+    e.preventDefault();
+    const newItem = document.getElementById("new-task-description").value 
+    console.log(newItem);
+    buildTask(newItem);
+    
+  })
+}); 
+function buildTask(task){
+  const li = document.createElement("li")
+  li.textContent = task
+
+  const button = document.createElement("button")
+  button.addEventListener('click' ,handleDelete);
+  button.textContent = 'X';
+
+  li.appendChild(button)
+  const theList = document.querySelector('#tasks')
+  theList.appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
